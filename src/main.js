d2b22cecd339a7eeb7ac5391953b1bb7482e72c1
Paola Rosano <paodiazglez.pdgr@gmail.com>
2022-08-12 09:29:30 -0500

    agrega fondo para la nav, avatares para las tarjetas y estilos

--- src/main.js
+++ src/main.js
@@ -5,15 +5,33 @@ import data from './data/athletes/athletes.js';
 let information = data.athletes;
 
 information.forEach(athletes => { 
+    if(athletes.gender == 'F') {
+        const html = `
+        <div class="card">
+        <div class="card-image"></div>
+        <div class="card-opacidad"></div>
+            <img src="./imagenes/sportswear-svgrepo-com.svg" alt="avatar-femel" class="avatar">
+                <h3>${athletes.name}</h3>
+                <p>Team: ${athletes.team}</p>
+                <p>Sport: ${athletes.sport}</p>
+        </div>
+            `
+    
+    container.insertAdjacentHTML('afterbegin', html) 
+} else {
+    const html = `
+        <div class="card">
+        <div class="card-image"></div>
+        <div class="card-opacidad"></div>
+            <img src="./imagenes/sportswear-svgrepo-com (1).svg" alt="avatar-male" class="avatar">
+                <h3>${athletes.name}</h3>
+                <p>Team: ${athletes.team}</p>
+            <p>Sport: ${athletes.sport}</p>
+        </div>
+            `
+    
+    container.insertAdjacentHTML('afterbegin', html) 
+}
 
-const html = `
-    <div class="card">
-    <div class="card-image"></div>
-        <h3>${athletes.name}</h3>
-        <p>Team: ${athletes.team}</p>
-        <p>Sport: ${athletes.sport}</p>
-    </div>
-        `
 
-container.insertAdjacentHTML('afterbegin', html)
 });
