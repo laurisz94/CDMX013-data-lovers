60731714acb9da1164716cc5439d0c275d7d9162
laurisz94 <lauriisz1994@gmail.com>
2022-08-24 18:15:45 -0500

    cambios en el main.js para que funcione los All de categorias

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-
 /* eslint-disable no-undef */
 import {filterGender, filterSport, filterTeam, orderName} from './data.js';
 //import athletes from './data/athletes/athletes.js';
@@ -30,11 +29,20 @@ container.insertAdjacentHTML('afterbegin', html);
 selectGender.addEventListener('change', (e) => {
     element.innerHTML =''
     html =''
+    
 
     let typeGender = filterGender(e.target.value, athletesNew)
-
+if (e.target.value === "F") {
     typeGender.forEach(athletes =>html+=generadorHTML(athletes));
+    }
 
+if (e.target.value === "M") { 
+    typeGender.forEach(athletes =>html+=generadorHTML(athletes));
+    }
+
+ if (e.target.value === "all")  {
+    athletesNew.forEach(athletes =>html+=generadorHTML(athletes));
+ }
 container.insertAdjacentHTML('afterbegin', html);
 
 });
@@ -45,8 +53,20 @@ selectSport.addEventListener('change', (e) => {
     html =''
 
     let typeSport = filterSport(e.target.value, athletesNew)
-
+if (e.target.value === "Rowing"){
     typeSport.forEach(athletes =>html+=generadorHTML(athletes));
+    }
+
+ if (e.target.value === "Gymnastics"){
+        typeSport.forEach(athletes =>html+=generadorHTML(athletes));
+        }
+
+if (e.target.value === "Taekwondo"){
+            typeSport.forEach(athletes =>html+=generadorHTML(athletes));
+            }
+if (e.target.value === "all")            
+    athletesNew.forEach(athletes =>html+=generadorHTML(athletes));
+
 
 container.insertAdjacentHTML('afterbegin', html);
 });
@@ -59,8 +79,25 @@ selectTeam.addEventListener('change', (e) => {
     let typeTeam = filterTeam(e.target.value, athletesNew);
 
     //console.log(filterTeam(e.target.value, athletesNew));
-
+if (e.target.value === "Italy") {
     typeTeam.forEach(athletes =>html+=generadorHTML(athletes));
+    }
+
+if (e.target.value === "Iran") {
+        typeTeam.forEach(athletes =>html+=generadorHTML(athletes));
+        }
+
+if (e.target.value === "Russia") {
+    typeTeam.forEach(athletes =>html+=generadorHTML(athletes));
+            }
+
+if (e.target.value === "Australia") {
+    typeTeam.forEach(athletes =>html+=generadorHTML(athletes));
+     }
+
+if (e.target.value === "all") {
+    athletesNew.forEach(athletes =>html+=generadorHTML(athletes));
+    }
 
     //console.log(typeTeam)
 container.insertAdjacentHTML('afterbegin', html);
@@ -71,24 +108,16 @@ order.addEventListener('change', (e) => {
     html = ''
 
     let orderAthletes = orderName(e.target.value, athletesNew);
-
+if (e.target.value === "AZ") {
     orderAthletes.forEach(athletes =>html+=generadorHTML(athletes));
+    }
+
+if (e.target.value === "ZA") {
+    orderAthletes.forEach(athletes =>html+=generadorHTML(athletes));
+        }
+
+if (e.target.value === "order")
+    athletesNew.forEach(athletes =>html+=generadorHTML(athletes));
 
     container.insertAdjacentHTML('afterbegin', html);
 })
-
-//const unicos = []*/
-
-//function calcularMedallas (datos) {
-//console.log(datos)
-
-//let atletasMedallas = []
-
-//datos.forEach(atleta => {
-//if (athletasMedallas no existe el atleta) {
-//empujar a atletasMEDALLAS{nombre:¨atleta.name, medallas:¨[]}
-//}
-//})   
-//}
-
-//calcularMedallas(information)
