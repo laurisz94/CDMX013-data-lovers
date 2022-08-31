d51ca4f49a5025c87724c9ca97ebc62132814b04
laurisz94 <lauriisz1994@gmail.com>
2022-08-31 18:18:59 -0500

    Footer con redes sociales y link

--- src/generadorHTML.js
+++ src/generadorHTML.js
@@ -5,7 +5,7 @@ const generadorHTML = (athletes)=>{
         <div class="card" id="container_female">
             <div class="card-image">
                 <div class="card-opacidad"></div>
-                    <img src="./imagenes/sportswear-svgrepo-com.svg" alt="avatar-female" class="avatar">
+                    <img src="src/imagenes/sportswear-svgrepo-com.svg" alt="avatar-female" class="avatar">
                 
                     <h4>${athletes.name.toUpperCase()}</h4>
                     <p>Team: ${athletes.team} <br> Sport: ${athletes.sport}</p>
@@ -19,7 +19,7 @@ const generadorHTML = (athletes)=>{
          <div class="card" id="container_male">
             <div class="card-image">
                 <div class="card-opacidad"></div>
-                    <img src="./imagenes/sportswear-svgrepo-com (1).svg" alt="avatar-male" class="avatar">
+                    <img src="src/imagenes/sportswear-svgrepo-com (1).svg" alt="avatar-male" class="avatar">
                 
                     <h4>${athletes.name.toUpperCase()}</h4>
                     <p>Team: ${athletes.team} <br> Sport: ${athletes.sport}</p>
